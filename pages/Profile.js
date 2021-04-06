1324ea0fe349492d211e4c4c9069e62cc69dda9c
unknown <manyuabi555@gmail.com>
2021-04-06 19:46:15 +0700

    first commit

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,137 +1,192 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {SafeAreaView, StyleSheet,Image, ScrollView} from 'react-native';
-import {Layout,Text, Card,  Divider, Button} from '@ui-kitten/components';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, Image, ScrollView} from 'react-native';
+import {Layout, Text, Card, Divider, Button} from '@ui-kitten/components';
+import {AsyncStorage} from '@react-native-async-storage/async-storage';
+import firestore from '@react-native-firebase/firestore';
 
 const Profile = ({navigation}) => {
+  const [bankData, setBankData] = useState([]);
+  const [interviews, setInterviews] = useState([]);
+  const [userID, setUserID] = useState('');
   // const navigateDetails = () => {
   //   navigation.navigate('Details');
   // };
 
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('uid');
+      if (value !== null) {
+        // value previously stored
+        // console.log(value);
+        setUserID(value);
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  const getInterviews = async () => {
+    var snapshot = await firestore()
+      .collection('candidates')
+      .onSnapshot(querySnapshot => {
+        const list = [];
+        querySnapshot.forEach(doc => {
+          const {cv, dob, email, fullname} = doc.data();
+          list.push({
+            id: doc.id,
+            cv: cv,
+            dob: dob,
+            email: email,
+            fullname: fullname,
+          });
+          console.log(list);
+        });
+        console.log(list);
+        setInterviews(list);
+        setBankData(list);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+    getInterviews();
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <Layout style={styles.container}>
-          <Layout style={styles.header} />
-          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
-          <Text style={styles.name}>John Doe</Text>
-         <Card style={styles.card}>
-         <Text style={styles.textLabel}>Name</Text>
-           <Text style={styles.text}>John Doe</Text>
-           <Divider />
+        <Layout style={styles.header} />
+        <Image
+          style={styles.avatar}
+          source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}
+        />
+        <Text style={styles.name}>John Doe</Text>
 
-           <Text style={styles.textLabel}>DOB</Text>
-           <Text style={styles.text}>jakarta 12-12-2001</Text>
-           <Divider />
+        {interviews &&
+          interviews.map(interview => {
+            return (
+              <Card style={styles.card}>
+                <Text style={styles.textLabel}>Name</Text>
+                <Text style={styles.text}>{interview.fullname}</Text>
+                <Divider />
 
-           <Text style={styles.textLabel}>Email</Text>
-           <Text style={styles.text}>Programmer@gmail.com</Text>
-           <Divider />
+                <Text style={styles.textLabel}>DOB</Text>
+                <Text style={styles.text}>{interview.email}</Text>
+                <Divider />
 
-            <Text style={styles.textLabel}>Age</Text>
-           <Text style={styles.text}>20</Text>
-           <Divider />
+                <Text style={styles.textLabel}>Email</Text>
+                <Text style={styles.text}>{interview.email}</Text>
+                <Divider />
 
-           <Text style={styles.textLabel}>Addres</Text>
-           <Text style={styles.text}>Jl baru</Text>
-           <Divider />
+                <Text style={styles.textLabel}>CV</Text>
+                <Image
+                  style={styles.avatar}
+                  source={interview.cv}
+                />
+                <Divider />
+                <Text style={styles.textLabel}>Password</Text>
+                <Text style={styles.text}>******</Text>
 
-           <Text style={styles.textLabel}>Last Education</Text>
-           <Text style={styles.text}>S1 Teknik Informatika</Text>
-           <Divider />
-           <Text style={styles.textLabel}>Password</Text>
-           <Text style={styles.text}>******</Text>
-           
-           
-           <Button style={styles.button} appearance="outline" status="primary">
-          Edit
-        </Button>
-           </Card>
-         
-          
+                <Button
+                  style={styles.button}
+                  appearance="outline"
+                  status="primary">
+                  Edit
+                </Button>
+              </Card>
+            );
+          })}
       </Layout>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
-  header:{
-    backgroundColor: '#00BFFF',
-    height:250,
+  header: {
+    padding: 10,
+    paddingBottom: 20,
+    backgroundColor: '#6155a6',
+    height: 175,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
   },
-  card:{
-    backgroundColor: '#bcb9c9',
+  card: {
+    backgroundColor: '#ffff',
     position: 'absolute',
-    marginTop:215,
-    marginRight:20,
-    marginLeft:20,
-    height:540,
-    width:350,
-    borderRadius:20,
+    marginTop: 205,
+    marginRight: 20,
+    marginLeft: 20,
+    height: 540,
+    width: 350,
+    borderRadius: 40,
   },
   avatar: {
-    width: 130,
-    height: 130,
+    width: 100,
+    height: 100,
     borderRadius: 63,
     borderWidth: 4,
     borderColor: 'white',
-    marginBottom:10,
-    alignSelf:'center',
+    marginBottom: 10,
+    alignSelf: 'center',
     position: 'absolute',
-    marginTop:30,
+    marginTop: 30,
   },
-  name:{
-    fontSize:22,
-    color:'#050505',
-    fontWeight:'600',
+  name: {
+    fontSize: 22,
+    color: '#ffffff',
+    fontWeight: '600',
     position: 'absolute',
-    marginTop:170,
-    alignSelf:'center',
+    marginTop: 130,
+    alignSelf: 'center',
   },
-  body:{
-    marginTop:40,
+  body: {
+    marginTop: 40,
   },
   bodyContent: {
     flex: 1,
     alignItems: 'center',
-    padding:30,
+    padding: 30,
   },
-  info:{
-    fontSize:16,
+  info: {
+    fontSize: 16,
     color: '#00BFFF',
-    marginTop:10,
+    marginTop: 10,
   },
-  description:{
-    fontSize:16,
+  description: {
+    fontSize: 16,
     color: '#696969',
-    marginTop:10,
+    marginTop: 10,
     textAlign: 'center',
   },
   buttonContainer: {
-    marginTop:10,
-    height:45,
+    marginTop: 10,
+    height: 45,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
     backgroundColor: '#00BFFF',
   },
-  text:{
-    marginLeft:20,
-    marginTop:10,
-    marginBottom:5,
+  text: {
+    marginLeft: 20,
+    marginTop: 10,
+    marginBottom: 5,
   },
-  textLabel:{
-    marginLeft:20,
-     fontSize:12,
-     marginTop:10,
-
+  textLabel: {
+    marginLeft: 20,
+    fontSize: 12,
+    marginTop: 10,
   },
   button: {
     margin: 5,
     width: '100%',
     height: 50,
+    borderRadius: 40,
   },
 });
 export default Profile;
--- pages/Results.js
+++ pages/Results.js
@@ -6,103 +6,10 @@ import {SafeAreaView, StyleSheet, ScrollView} from 'react-native';
 import {Divider, Layout, TopNavigation, Text} from '@ui-kitten/components';
 import SearchInput from '../components/SearchInput';
 import Cards from '../components/Cards';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import firestore from '@react-native-firebase/firestore';
 
 const Results = ({navigation}) => {
-  const navigateDetails = (id, job) => {
-    navigation.navigate('ResultsDetail', {
-      id: id,
-      job: job,
-    });
-  };
-
-  const bankData = [
-    {
-      id: 1,
-      jobName: 'Front-End Engineer',
-      jobDescription: 'Front-End Engineer',
-      announcement: '01 December 2021',
-    },
-    {
-      id: 2,
-      jobName: 'Back-End Engineer',
-      jobDescription: 'Back-End Engineer',
-      announcement: '02 December 2021',
-    },
-    {
-      id: 3,
-      jobName: 'Full-Stack Engineer',
-      jobDescription: 'Full-Stack Engineer',
-      announcement: '03 December 2021',
-    },
-    {
-      id: 4,
-      jobName: 'DevOps Engineer',
-      jobDescription: 'DevOps Engineer',
-      announcement: '04 December 2021',
-    },
-    {
-      id: 5,
-      jobName: 'AI Engineer',
-      jobDescription: 'AI Engineer',
-      announcement: '05 December 2021',
-    },
-    {
-      id: 6,
-      jobName: 'Big Data Engineer',
-      jobDescription: 'Big Data Engineer',
-      announcement: '06 December 2021',
-    },
-  ];
-
-  const [jobs, setJobs] = useState([
-    {
-      id: 1,
-      jobName: 'Front-End Engineer',
-      jobDescription: 'Front-End Engineer',
-      announcement: '01 December 2021',
-    },
-    {
-      id: 2,
-      jobName: 'Back-End Engineer',
-      jobDescription: 'Back-End Engineer',
-      announcement: '02 December 2021',
-    },
-    {
-      id: 3,
-      jobName: 'Full-Stack Engineer',
-      jobDescription: 'Full-Stack Engineer',
-      announcement: '03 December 2021',
-    },
-    {
-      id: 4,
-      jobName: 'DevOps Engineer',
-      jobDescription: 'DevOps Engineer',
-      announcement: '04 December 2021',
-    },
-    {
-      id: 5,
-      jobName: 'AI Engineer',
-      jobDescription: 'AI Engineer',
-      announcement: '05 December 2021',
-    },
-    {
-      id: 6,
-      jobName: 'Big Data Engineer',
-      jobDescription: 'Big Data Engineer',
-      announcement: '06 December 2021',
-    },
-  ]);
-
-  const filterData = data => {
-    let currentJobs = [...jobs];
-    let newJobs = currentJobs.filter(job => job.jobName === data);
-    if (newJobs.length > 0) {
-      setJobs(newJobs);
-    } else {
-      setJobs(bankData);
-    }
-  };
-
   return (
     <SafeAreaView style={{flex: 1}}>
       <Layout
